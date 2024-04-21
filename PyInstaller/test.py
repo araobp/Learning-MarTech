7823efb5dce6b177bff9e4d4d70e0eacc4f0f8d5
araobp <araobp@users.noreply.github.com>
2024-04-21 15:50:55 +0900

    Update test.py

--- PyInstaller/test.py
+++ PyInstaller/test.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+from flask import Flask
+import webbrowser
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+  url = "http://127.0.0.1:5000"
+  webbrowser.open(url, new=0, autoraise=True)
+  app.run()
+

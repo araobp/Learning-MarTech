13a859ae78eae7864cc1a5568d70c95d921e4de2
araobp <araobp@users.noreply.github.com>
2024-04-30 23:11:08 +0900

    update

--- pdf_highlight/App/controller/controller.py
+++ pdf_highlight/App/controller/controller.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, request
 from models import models
 
 main = Blueprint('main', __name__)
@@ -15,3 +15,10 @@ def index():
 def sources():
   sources = models.get_sources()
   return jsonify(sources)
+
+@main.route('/search')
+def search():
+  base_url = request.args.get('base_url', default = None, type = str)
+  keywords = request.args.get('keywords', default = None, type=str)
+  keywords_ = [e.strip() for e in keywords.split(',')]
+  return jsonify(keywords_)
--- pdf_highlight/App/models/models.py
+++ pdf_highlight/App/models/models.py
@@ -7,7 +7,7 @@ DB_PATH = os.path.join(cwd, "../../database/search.db")
 def get_sources():
   with sqlite3.connect(DB_PATH) as conn:
     cur = conn.cursor()
-    sources = cur.execute('SELECT * FROM source').fetchall()
+    sources = cur.execute('SELECT * FROM sources').fetchall()
   return sources
 
 if __name__ == '__main__':
